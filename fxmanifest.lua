4b934675562c83963e61825d30b485e7a466a8c7
Txgls <businesstoggled@gmail.com>
2025-03-26 16:25:56 +0000

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Txgls'
+description 'Police evidence script. Currently featuring bullet casings.'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+lua54 'yes'
