ee3983e1f52e45b41c3add6a96e10a9df7856a92
Txgls <businesstoggled@gmail.com>
2025-03-26 18:52:57 +0000

    Updated config to reflect version 1.0.1

--- config.lua
+++ config.lua
@@ -1,15 +1,21 @@
 Config = Config or {}
 
 Config.Evidence = {
-
-    IncludeWeaponSerial = true,     -- Include or to not include unique serial IDs to casings.
-    SerialNumberLength = 8,         -- Length of the serial ID.
-    EvidenceExpireTime = 30,        -- Minutes
+    IncludeWeaponSerial = true,
+    SerialNumberLength = 8,            -- Length of the serial ID for bullet casings.
     
-    CollectDistance = 2.0,
+    EvidenceExpireTime = 30,           -- Minutes.
+    CollectDistance = 1.0,             -- Keep this low.
+    
     EvidenceBagItem = 'evidence_bag',
     BulletCasingItem = 'casing',
-    DropChance = 85,
+    BloodSampleItem = 'blood_sample',
+    
+    DropChance = 85,                   -- Bullet casings.               
+    BloodDropChance = 70,              -- Blood samples.
+    
+    MinBleedDamage = 15,               -- Minimum damage to activate a blood sample.
+    BloodTypes = {"A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"},
     
     BlacklistedWeapons = {
         `WEAPON_RAILGUN`,
@@ -17,14 +23,17 @@ Config.Evidence = {
         `WEAPON_FIREEXTINGUISHER`,
         `WEAPON_PETROLCAN`,
         `WEAPON_HATCHET`,
-        `WEAPON_BAT`
+        `WEAPON_BAT`,
+        -- Additional weapons can be added here.
     }
 }
 
 Config.Notifications = {
-    NoEvidenceBag = "You need an evidence bag to collect this!",
-    EvidenceCollected = "Evidence collected!",
-    EvidenceExpired = "Evidence has expired and is no longer collectable!"
+    NoEvidenceBag = "You need an evidence bag to collect this",
+    EvidenceCollected = "Evidence collected",
+    BloodCollected = "Blood sample collected",
+    EvidenceExpired = "Evidence has expired and is no longer collectable",
+    NotPolice = "You're not authorized to collect evidence"
 }
 
-Config.Debug = false
+Config.Debug = false                   -- Leave this false unless you know what you are doing.
